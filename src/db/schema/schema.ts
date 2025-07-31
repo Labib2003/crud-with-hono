7af8ba057d70e87736196097e736266892e02c20
Labib2003 <labib.salimi30@gmail.com>
2025-07-31 20:09:53 +0600

    feat: drizzle orm configured

---
+++ src/db/schema/schema.ts
@@ -0,0 +1,16 @@
+import { integer, pgEnum, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const CategoriesEnum = pgEnum("categories", [
+  "tech",
+  "health",
+  "lifestyle",
+  "education",
+]);
+
+export const postsTable = pgTable("posts", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+
+  title: varchar({ length: 255 }).notNull(),
+  category: CategoriesEnum(),
+  body: varchar({ length: 2047 }).notNull(),
+});
